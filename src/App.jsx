3ffda70a3cadbb315d15fbbf45c4823080515c70
Kevin Alberto Palma Ralda <kapalma05@gmail.com>
2022-06-05 12:56:09 -0600

    :sparkles: Create component ListOfGifs
    Create component ListOfGifs

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,12 @@
-import { useState, useEffect } from 'react';
-import Gif from './components/Gif';
-import { getGifs } from './services/getGifs';
+import ListOfGifs from './containers/ListOfGifs';
 import './styles/App.scss'
 
 function App() {
-  // Crear el estado de los gifs
-  const [gifs, setGifs] = useState([])
-
-  // Efecto de carga de los gif al renderizar el componente
-  useEffect(() => {
-    console.log('actualizando los gifs')
-    // Utilizando el fetch para obtener los gifs
-    getGifs({ keyword: 'baby groot' }).then(gifs => setGifs(gifs))
-  }, [])
   
   return (
     <div className="App">
       <section className="App-content">
-        {
-          gifs.map(({ id, title, url }) => <Gif 
-            key={id} 
-            title={title} 
-            url={url} 
-          />)
-        }
+        <ListOfGifs keyword='ps5' />
       </section>
     </div>
   );
--- src/containers/ListOfGifs.jsx
+++ src/containers/ListOfGifs.jsx
@@ -1,7 +1,24 @@
-const ListOfGifs = () => {
-  return (
-    
-  )
+import { useState, useEffect } from "react"
+import { getGifs } from "../services/getGifs"
+import Gif from "../components/Gif"
+
+const ListOfGifs = ({ keyword }) => {
+  // Crear el estado de los gifs
+  const [gifs, setGifs] = useState([])
+
+  // Efecto de carga de los gif al renderizar el componente
+  useEffect(() => {
+    console.log('actualizando los gifs')
+    // Utilizando el fetch para obtener los gifs
+    getGifs({ keyword }).then(gifs => setGifs(gifs))
+  }, [keyword])
+
+  return gifs.map(({ id, title, url }) => 
+    <Gif 
+      key={id} 
+      title={title} 
+      url={url} 
+    />)
 }
 
 export default ListOfGifs
