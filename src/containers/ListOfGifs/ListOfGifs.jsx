1b98a550be465b2152a7aed1981e97c128ddd047
Kevin Palma KodePrint <kapalma05@gmail.com>
2022-06-09 15:44:35 -0600

    :lipstick: add responsive layaut mansory-css
    add responsive layaut mansory-css

--- src/containers/ListOfGifs/ListOfGifs.jsx
+++ src/containers/ListOfGifs/ListOfGifs.jsx
@@ -1,21 +1,39 @@
+import { useRef, useEffect } from 'react'
+import Masonry from 'react-masonry-css'
+
 // Import components
 import Gif from "components/Gifs/Gif"
 // Import CSS
 import "./ListOfGifs.scss"
 
 const ListOfGifs = ({ gifs }) => {
+  
+  const breakpointColumnsObj = {
+    default: 4,
+    1100: 3,
+    700: 2,
+    500: 1
+  };
+  
+
+  const container = useRef('')
 
   return (
-    <div className="ListOfGifs">
-      {
-        gifs.map(({ id, title, url }) => 
-          <Gif 
-            id={id}
-            key={id} 
-            title={title} 
-            url={url} 
-          />)
-      }
+    <div ref={container} className="ListOfGifs">
+      <Masonry
+        breakpointCols={breakpointColumnsObj}
+        columnClassName='my-masonry-grid_column'
+      >
+        {
+          gifs.map(({ id, title, url }) => 
+            <Gif 
+              id={id}
+              key={id} 
+              title={title} 
+              url={url} 
+            />)
+        }
+      </Masonry>
     </div>
   )
 }
